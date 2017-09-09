a5b408cf062c38673a088ef0b0dbfa404248d16f
Gergana <gergana.kara@gmail.com>
2017-09-09 12:23:54 +0200

    Initial Commit

---
+++ Aufgaben_1/main.cpp
@@ -0,0 +1,17 @@
+//
+//  main.cpp
+//  Aufgaben_1
+//
+//  Created by Gergana Karadzhova on 9/9/17.
+//  Copyright © 2017 Gergana Karadzhova. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, nice World!\n";
+    printf("hi\n");
+           
+    return 0;
+}
