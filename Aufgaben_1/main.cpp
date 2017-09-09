e37e2a5d234c967ade36d80452db40f3d27130d5
Gergana <gergana.kara@gmail.com>
2017-09-09 15:39:10 +0200

    Task 2 - pg 57

--- Aufgaben_1/main.cpp
+++ Aufgaben_1/main.cpp
@@ -6,10 +6,11 @@
 //  Copyright © 2017 Gergana Karadzhova. All rights reserved.
 //
 
-#include <iostream>
+/*#include <iostream>
 using namespace std;
 
 int main(int argc, const char * argv[]) {
+    // task 1 pg 56
     int var = 10;
     char c = 'G';
     float a = 3.1;
@@ -21,5 +22,27 @@ int main(int argc, const char * argv[]) {
     printf("hi\n");
            
     return 0;
+    
+  */
+    // task 2 pg 57 - F to C
+    // c = ((f -32) * 5/9)
+#include <stdio.h>
+#include <iostream>
+
+using namespace std;
+
+int main( int argc, char* argv[]){
+    double C = 0.0;
+    double F = 20.0;
+    printf("The temperature in F is ");
+    
+    scanf_s("%lf", &F, 1);
+    
+    C = (F - 32) * 5.0/9.0;
+    
+    printf("%4.2f Grad Fahrenheit = %4.2f Grad Celsium \n", F, C);
+    return 0;
 }
+
+    
 
