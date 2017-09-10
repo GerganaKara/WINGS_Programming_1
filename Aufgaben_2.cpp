66680e85367a1eff427dac287d3faef307c06fd8
Gergana <gergana.kara@gmail.com>
2017-09-10 15:04:36 +0200

    e ^  Z

--- Aufgaben_2.cpp
+++ Aufgaben_2.cpp
@@ -4,11 +4,11 @@
 //
 //  Created by Gergana Karadzhova on 9/10/17.
 //  Copyright © 2017 Gergana Karadzhova. All rights reserved.
-//
+/*
 
 #include "Aufgaben_2.hpp"
 
-// IF Anweisungen
+// 1. IF Anweisungen
 
 #include <iostream>
 using namespace std;
@@ -36,3 +36,41 @@ int main()
     
     return 0;
 }
+*/
+// 2. While Anweisung e ˆ z
+
+#include <iostream>
+using namespace std;
+#include <math.h>
+#include <stdio.h>
+
+// declare recursive function
+int fak(int);
+int fak (int n) {
+    
+    if ( n>0)
+        return n * fak(n-1);
+    else
+        return 1;
+}
+// main program
+int main () {
+    
+    double Z, e = 1.00;
+    int N;
+    
+    cout << "Enter positive integer N: ";
+    cin >> N;
+    
+    cout << "Enter real number Z: ";
+    cin >> Z;
+    
+    for ( int i = 1; i <= N; i++) {
+        
+        e += (pow(Z,i)/fak(i));
+        cout << e << endl;
+    }
+    
+    cout << "e ^ Z equals : \n" << e << endl;
+
+    }
