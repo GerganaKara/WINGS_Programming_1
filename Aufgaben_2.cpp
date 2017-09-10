bb07d5d8776407aa90b300a13fb16e05d0651228
Gergana <gergana.kara@gmail.com>
2017-09-10 13:45:58 +0200

    adding new file

---
+++ Aufgaben_2.cpp
@@ -0,0 +1,10 @@
+//
+//  Aufgaben_2.cpp
+//  Aufgaben_1
+//
+//  Created by Gergana Karadzhova on 9/10/17.
+//  Copyright © 2017 Gergana Karadzhova. All rights reserved.
+//
+
+#include "Aufgaben_2.hpp"
+
---
+++ Aufgaben_2.hpp
@@ -0,0 +1,14 @@
+//
+//  Aufgaben_2.hpp
+//  Aufgaben_1
+//
+//  Created by Gergana Karadzhova on 9/10/17.
+//  Copyright © 2017 Gergana Karadzhova. All rights reserved.
+//
+
+#ifndef Aufgaben_2_hpp
+#define Aufgaben_2_hpp
+
+#include <stdio.h>
+
+#endif /* Aufgaben_2_hpp */
