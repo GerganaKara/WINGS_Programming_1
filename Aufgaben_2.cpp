01fb40cfaa21906fa42fa18e4a6696c5248abf48
Gergana <gergana.kara@gmail.com>
2017-09-10 14:16:40 +0200

    If Anweisungen

--- Aufgaben_2.cpp
+++ Aufgaben_2.cpp
@@ -5,7 +5,34 @@
 //  Created by Gergana Karadzhova on 9/10/17.
 //  Copyright © 2017 Gergana Karadzhova. All rights reserved.
 //
-task 2
 
 #include "Aufgaben_2.hpp"
 
+// IF Anweisungen
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n, i;
+    bool isPrime = true;
+    
+    cout << "Enter a positive integer: ";
+    cin >> n;
+    
+    for(i = 2; i <= n / 2; ++i)
+    {
+        if(n % i == 0)
+        {
+            isPrime = false;
+            break;
+        }
+    }
+    if (isPrime)
+        cout << "This is a prime number \n";
+    else
+        cout << "This is not a prime number \n";
+    
+    return 0;
+}
