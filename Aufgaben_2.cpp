f043be3fb5b93e62a96cf47187f9648b9c73058d
Gergana <gergana.kara@gmail.com>
2017-09-21 14:40:00 +0200

    recursive with parameter

--- Aufgaben_2.cpp
+++ Aufgaben_2.cpp
@@ -74,9 +74,10 @@ int main () {
     cout << "e ^ Z equals : \n" << e << endl;
 
     }
-*/
 
 // Switch Anweisung pg 91
+
+// version with IF
 
 #include <iostream>
 using namespace std;
@@ -102,4 +103,83 @@ int main() {
     
 }
 
+//version with Switch
+#include <iostream>
+using namespace std;
+#include <stdio.h>
 
+int main(){
+    
+    int Z;
+    cout << "enter a number, int: ";
+    cin >> Z;
+    
+    switch ( Z >= 0 && Z <= 10) {
+        case true :
+                cout << Z << " is between 0 and 10 \n";
+                break;
+        case false:
+            cout << Z << " is not between 0 and 10 \n";
+                break;
+      
+    }
+    
+    switch(Z % 2)
+        {
+                //If n%2 == 0
+            case 0: printf("Number is Even\n");
+                break;
+                //Else if n%2 != 0
+            case 1: printf("Number is Odd\n");
+                break;
+        }
+    }
+*/
+
+// 5. Funktionen Fakultät Berechnung
+#include <iostream>
+using namespace std;
+#include <stdio.h>
+
+//iterative
+int fak_iter ( int n) {
+    
+    int ret = 1;
+    
+    for (int i =2; i <= n; ++i)
+    { ret *= i;}
+    
+    return ret;
+}
+
+// recursive
+
+int fak_rec ( int n) {
+    
+    if (n > 0)
+        return n* fak_rec(n -1 );
+    else
+        return 1;
+}
+
+// recursive with Referenzparameter
+
+int fact_ref(int &n)
+{
+    if (&n > 1)
+    {
+        int tmp = &n - 1;
+        fact_ref(tmp);
+        &n *= tmp;
+    }
+}
+
+int main () {
+    int Z;
+    cout << "Enter a number : ";
+    cin >> Z;
+    cout << "Recursive calc result: "<< fak_rec (Z)<< endl;
+    cout << "Iterative calc result: "<< fak_iter (Z)<< endl;
+    int factorial = fact_ref(&Z);
+    cout << "Factorial by rec and ref is: "<< factorial << endl;
+}
